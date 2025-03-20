d0cd70d687fdcbfdddcb35514ce4ef5faf1a86c8
Shahd Elhlwaney <ShahdElhlwaney@example.com>
2025-03-21 01:13:15 +0200

    create login with mobile number

--- js/views/loginView.js
+++ js/views/loginView.js
@@ -4,31 +4,62 @@ class LoginView extends AuthView{
     _parentElement=document.querySelector('.form-container');
     _authWith=document.querySelector('.auth-with');
     _h1=document.querySelector('.auth-content').querySelector('h1');
+    _emailInputEl=this._parentElement.querySelector('.form__email');
+    constructor(){
+        super();
+        this.addLoginWithPhoneHandlerRender();
+    }
     _generateMarkup(){
         return ` <div class="login-nav">
-                    <div class="login-nav__email"><h3>E-mail</h3></div>
-                    <div class="login-nav__mobile"><h3>Mobile Number</h3></div>
+                    <div class="login-nav__email click"><h3>E-mail</h3></div>
+                    <div class="login-nav__mobile unclick"><h3>Mobile Number</h3></div>
                     
-                 </div>   
+                 </div>  
                     <form class="form">
+                        <input type="tel" placeholder="Phone"class="form__input form__phone hidden">
                         <input type="email" placeholder="Email"class="form__input form__email">
                         <input type="password" placeholder="Password"class="form__input form__password">
                         <div class="remember-me">
                             <input type="checkbox" id="remember_me" name="remember_me">
                             <label> Remember me</label>
                         </div>
-                        <p class="forget-pass">Forget Password</p>
+                        <p class="forget-pass">Forget Password?</p>
                         <button>Continue</button>
                         `;
                         
     }
     addLoginNavHandlerRender(handler){
         this._authNav.addEventListener('click',()=>{
+            
             this._authWith.firstElementChild.textContent='Log in with';
             this._authNav.textContent=' Sign up';
             this._h1.textContent='Welcome Back';
             handler();
         })
        }
+     _toggleNavEls(){
+       this._parentElement.querySelector('.form__email').classList.toggle('hidden');
+       this._parentElement.querySelector('.form__phone').classList.toggle('hidden');
+
+    } 
+    _changeMobileNavClickedStyle(){
+      this._parentElement.querySelector('.login-nav__mobile').classList.add('click');
+      this._parentElement.querySelector('.login-nav__mobile').classList.remove('unclick');
+      this._parentElement.querySelector('.login-nav__email').classList.add('unclick');
+      this._parentElement.querySelector('.login-nav__email').classList.remove('click');
+    }
+        
+    
+
+    
+    addLoginWithPhoneHandlerRender(){
+        this._parentElement.addEventListener('click',(e)=>{
+            const mobileNavEl=e.target.closest('.login-nav__mobile');
+            if(!mobileNavEl)return;
+            this._toggleNavEls();
+            this._changeMobileNavClickedStyle();
+        })
+
+    }   
 }
 export default new LoginView();
